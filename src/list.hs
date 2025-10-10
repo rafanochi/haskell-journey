fdacad4cfa0aa19536a3824c01321ecee701c195
Rafa <zawkindev@gmail.com>
2025-10-10 22:24:12 +0500

    feat: unzip func

--- src/list.hs
+++ src/list.hs
@@ -15,8 +15,16 @@ zipp :: [a] -> [b] -> [(a, b)]
 zipp [] [] = []
 zipp (x : xs) (y : ys) = (x, y) : zipp xs ys
 
+(+~) :: ([a], [b]) -> ([a], [b]) -> ([a], [b])
+(xs, ys) +~ (as, bs) = (xs ++ as, ys ++ bs)
+
+unzipp :: [(a, b)] -> ([a], [b])
+unzipp [] = ([],[])
+unzipp ((x, y) : xys) = ([x], [y]) +~ unzipp xys
+
 main :: IO ()
 main = do
   print (maxi [5, 2, 4, 9])
   print (sumi [5, 2, 4, 9])
   print (zipp [1, 2, 3] [True, False, False])
+  print (unzipp [(1, True), (2, False), (3, False)])
