cc795b554b3e571bb218b11e1f75f9a5a765ff8b
Rafa <zawkindev@gmail.com>
2025-10-10 21:49:38 +0500

    feat: zip func

--- src/list.hs
+++ src/list.hs
@@ -5,12 +5,18 @@ x ~ y
 
 maxi :: [Int] -> Int
 maxi [x] = x
-maxi (x:xs) = x ~ maxi xs
+maxi (x : xs) = x ~ maxi xs
 
-sumi:: [Int] -> Int
+sumi :: [Int] -> Int
 sumi [x] = x
-sumi (x:xs) = x + sum xs
+sumi (x : xs) = x + sum xs
 
-main::IO()
+zipp :: [a] -> [b] -> [(a, b)]
+zipp [] [] = []
+zipp (x : xs) (y : ys) = (x, y) : zipp xs ys
+
+main :: IO ()
 main = do
-    print(maxi[5,2,4,9])
+  print (maxi [5, 2, 4, 9])
+  print (sumi [5, 2, 4, 9])
+  print (zipp [1, 2, 3] [True, False, False])
