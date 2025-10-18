7a4161c160b3e316296bc4c3e03673a03d7db67a
rafanochi <zawkindev@gmail.com>
2025-10-18 21:16:57 +0500

    use monad with Map

--- src/Map.hs
+++ src/Map.hs
@@ -6,10 +6,18 @@ values = Map.fromList [("z", 3), ("a", 6)]
 nv :: Map.Map String Integer
 nv = Map.insert "x" 7 values
 
-nvv = Map.insert "y" 10 $ Map.insert "m" 0 values 
+nvv :: Map.Map String Integer
+nvv = Map.insert "y" 10 $ Map.insert "m" 0 values
+
+cnvv = Map.insert "y" 100000 nvv
+
+withdraw :: String -> Int -> Map.Map String Int -> Maybe (Map.Map String Int)
+withdraw account amount database =
+  Map.lookup account database >>= (\x -> Just (Map.insert account (x - amount) database))
 
 main :: IO ()
 main = do
   print (Map.lookup "z" values)
   print (Map.lookup "i" values)
   print (Map.lookup "m" nvv)
+  print (Map.lookup "x" cnvv)
