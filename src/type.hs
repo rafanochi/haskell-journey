f45f8f2dc1a67a25672ade433139c432fcb2376e
rafanochi <zawkindev@gmail.com>
2025-11-14 10:19:21 +0500

    more functions for LinkedList

--- src/type.hs
+++ src/type.hs
@@ -6,3 +6,11 @@ ihead (Node x _) = x
 itail :: LinkedList a -> a
 itail (Node x Empty) = x
 itail (Node _ xs) = itail xs
+
+inull :: LinkedList a -> Bool
+inull Empty = True
+inull _ = False
+
+ilength :: LinkedList a -> Int
+ilength Empty = 0
+ilength (Node x xs) = ilength xs + 1 
